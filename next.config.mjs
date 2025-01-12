868190602b902383c4956779011b071811cc256f
boobalan <boobalanm07@gmail.com>
2025-01-12 15:13:47 +0530

    Fix: URL /home to redirect to main page

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,15 @@ const nextConfig = {
   images: {
     domains: ['admin.xevy.io'], // Add the external domain here
   },
+  async redirects() {
+    return [
+      {
+        source: '/home',
+        destination: '/',
+        permanent: true, // You can set this to false if it's a temporary redirect
+      },
+    ]
+  },
 };
 
 export default nextConfig;
